42a2f2bf6f778c26c7bfba52ffcc79de64555714
abhishek-jain04 <abhishekjain2465@gmail.com>
2021-03-22 16:11:49 +0530

    Add project files.

---
+++ DemoProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public void fullname()
+        {
+            string fname = "Abhishek";
+            string lname = "Jain";
+            Console.WriteLine(fname + " " + lname);
+        }
+    }
+}
