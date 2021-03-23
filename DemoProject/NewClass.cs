eaf39657f48cccc165716d0709271533fcc44b32
abhishek-jain04 <abhishekjain2465@gmail.com>
2021-03-23 09:51:24 +0530

    AJ jenkins trial

---
+++ DemoProject/NewClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class NewClass
+    {
+        public void fn1()
+        {
+            Console.WriteLine("Abhishek trying to build the sln through jenkins.");
+        }
+    }
+}
