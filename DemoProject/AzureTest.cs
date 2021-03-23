c6316d33c9a004b36dc0fa8fba7a23ea175d3838
abhishek-jain04 <abhishekjain2465@gmail.com>
2021-03-23 14:26:46 +0530

    Azure Devops Testing by AJ

---
+++ DemoProject/AzureTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class AzureTest
+    {
+        public void AzureDevopsTest()
+        {
+            Console.WriteLine("Azure Devops CI/CD by AJ");
+        }
+    }
+}
